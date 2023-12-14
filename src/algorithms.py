86c398950351bfa4b274876ae1fcaef5a7e947a9
Xueyi Chee <xueyi.chee@gmail.com>
2023-12-15 04:28:33 +1100

    wrote main function's stub

---
+++ src/algorithms.py
@@ -0,0 +1,8 @@
+from customTypes.sessionTypes import *
+
+def allocateStudySessions(
+    subs: SubjectsToAllocate, idealSessionMinutes: int, studySessions: list[StudySession]
+) -> AllocatedStudySessions:
+    # to implement
+    ret = AllocatedStudySessions()
+    return ret
